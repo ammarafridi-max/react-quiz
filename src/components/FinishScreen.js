033739abedbccdb5deff52743020af02c22b0ef8
ammarafridi-max <ammar.afridi95@gmail.com>
2024-06-24 07:27:17 +0400

    Initial commit

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,24 @@
+export default function FinishScreen({
+  points,
+  maxPossiblePoints,
+  highscore,
+  dispatch,
+}) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  return (
+    <>
+      <p className="result">
+        You scored {points} out of {maxPossiblePoints} ({Math.ceil(percentage)}
+        %)
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart
+      </button>
+    </>
+  );
+}
---
+++ src/components/Timer.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+export default function Timer({ dispatch, secondsRemaining }) {
+  const minutes = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      dispatch({ type: "tick" });
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, []);
+
+  return (
+    <div className="timer">
+      {minutes < 10 ? `0${minutes}` : minutes}:
+      {seconds < 10 ? `0${seconds}` : seconds}
+    </div>
+  );
+}
